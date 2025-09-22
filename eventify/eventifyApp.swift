e3ba5eda4c3cc457fbae99f2ea8418e8bf38a91f
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-09-22 02:51:22 +0100

    Initial Commit

---
+++ eventify/eventifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventifyApp.swift
+//  eventify
+//
+//  Created by BSTAR on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct eventifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
